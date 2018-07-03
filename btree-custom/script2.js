939453590b59717fa7f7e2e4155d3728e0a2476b
Terrance Li <terranceli.terbear@gmail.com>
2018-07-02 19:06:32 -0700

    added expand and collapse login

--- btree-custom/script2.js
+++ btree-custom/script2.js
@@ -12,15 +12,22 @@ var svg = d3.select('body').append('svg:svg')
 
 var accumulator = (a, b) => a + b;
 
-var injectParent = (node) => {
-  // also add a code for every node for group identification
+var getNodeCode = (node) => {
   var nodeCode = '-' + node.values.reduce(accumulator);
   node.children.forEach((child) => {
     nodeCode += '-' + child.values.reduce(accumulator);
+  });
+
+  return nodeCode.substring(1);
+}
+
+var injectParent = (node) => {
+  // also add a code for every node for group identification
+  node.children.forEach((child) => {
     child.parent = node;
     injectParent(child);
   });
-  node.code = nodeCode.substring(1);
+  node.code = getNodeCode(node);
 }
 
 /**
@@ -36,7 +43,7 @@ var convertToMatrix = (data) => {
   matrix = [];
   matrix.push([]);
 
-  while(stack.length > 0) {
+  while (stack.length > 0) {
     var current = stack.splice(0, 1)[0];
     if (current == null) {
       depth++;
@@ -67,9 +74,15 @@ var draw = (matrix) => {
       var nodeWidth = node.values.length * keySize;
       var x = xCenter - nodeWidth / 2;
 
+      var attachDOM = svg;
+      if (node.parent != null) {
+        attachDOM = node.parent.group.append('svg:g')
+          .attr('id', node.code);
+      }
+
       // draw the node itself
       node.values.forEach((key, keyIndex, keys) => {
-        svg.append('svg:rect')
+        attachDOM.append('svg:rect')
           .attr('height', keySize)
           .attr('width', keySize)
           .attr('x', keySize * keyIndex + x)
@@ -78,31 +91,55 @@ var draw = (matrix) => {
           .attr('stroke', 'steelblue')
           .attr('stroke-width', 4);
 
-        svg.append('svg:text')
-          .attr('x', keySize * (keyIndex + 0.5) + x)
-          .attr('y', y + keySize / 1.5)
-          .attr('font-family', 'Lato')
-          .attr('font-size', 24)
-          .attr('fill', 'black')
-          .attr('stroke', 'black')
-          .style('text-anchor', 'middle')
-          .text(function () {
-            return key
-          });
+        attachDOM.append('svg:text')
+        .attr('x', keySize * (keyIndex + 0.5) + x)
+        .attr('y', y + keySize / 1.5)
+        .attr('font-family', 'Lato')
+        .attr('font-size', 24)
+        .attr('fill', 'black')
+        .attr('stroke', 'black')
+        .style('text-anchor', 'middle')
+        .text(() => {
+          return key
+        });
       });
 
       // draw the node's children
       node.children.forEach((child, childIndex, children) => {
-        svg.append('svg:circle')
+        var circle = attachDOM.append('svg:circle')
           .attr('cx', x + (keySize * childIndex))
           .attr('cy', y + keySize)
           .attr('r', keySize / 8)
           .attr('fill', child.expanded ? 'steelblue' : 'white')
-          .attr('stroke', 'green')
-          .on('click', () =>{
+          .attr('stroke', 'steelblue')
+          .attr('stroke-width', 4)
+          .on('click', () => {
+            if (child.expanded) {
+              child.expanded = false;
+              d3.select('[id="' + getNodeCode(child) + '"]')
+              .transition()
+                .style('opacity', 0)
+                .duration(300);
 
+              circle.transition()
+                .style('fill', 'white')
+                .duration(300);
+            } else {
+              child.expanded = true;
+              d3.select('[id="' + getNodeCode(child) + '"]')
+              .transition()
+                .style('opacity', 1)
+                .duration(300);
+
+              circle.transition()
+                .style('fill', 'steelblue')
+                .duration(300);
+            }
+            
           });
       });
+
+      node.group = attachDOM;
     });
   });
 }
@@ -110,7 +147,7 @@ var draw = (matrix) => {
 /**
  * Reads the json data from file input
  */
-d3.json('data-btree.json', function(data) {
+d3.json('data-btree.json', (data) => {
   // inject parent data
   injectParent(data.root);
 
